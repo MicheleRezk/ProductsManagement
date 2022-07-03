da1efe6c19ea6997a44299c5c15bf896883e1043
DESKTOP-2J4MC7J\Michele George <michelerezk2015@gmail.com>
2022-07-03 13:25:44 +0200

    Adding MongoRepo

---
+++ Complevo.ProductsManagement/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Complevo.ProductsManagement.Common
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<T> GetAsync(Guid id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(Guid id);
+    }
+}
